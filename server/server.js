0b7a33c343c42ed4162e5929db4987a3ce2e3b39
Pedro Henrique <pedrocontahf@gmail.com>
2025-03-24 16:39:44 -0300

    Server conectado ao banco de dados MongoDB

--- server/server.js
+++ server/server.js
@@ -1,10 +1,14 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const path = require('path')
+require('dotenv').config();
 
 const app = express()
 const PORT = 3000
 
 app.use(express.json())
+
+mongoose.connect(`mongodb+srv://pedrocontahf:${process.env.DB_PASSWORD}@cluster.jf1b1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster`);
 
 app.use(express.static(path.join(__dirname, '../src')))
 
