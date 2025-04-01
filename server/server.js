5b0a02adf2d18acc97011919a360eda6dab67ecb
Pedro Henrique <pedrocontahf@gmail.com>
2025-04-01 15:39:47 -0300

    Adicionada verificação de login utilizando express-session

--- server/server.js
+++ server/server.js
@@ -1,9 +1,18 @@
 const express = require('express')
+const session = require('express-session')
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
 const path = require('path')
 const Login = require('./models/model')
 require('dotenv').config();
+
+function verifySession (req,res, next) {
+    if (req.session.user) {
+        next();
+    } else {
+        res.redirect('/');
+    }
+}
 
 const app = express()
 const PORT = 3000
@@ -12,13 +21,19 @@ app.use(express.json())
 
 mongoose.connect(`mongodb+srv://pedrocontahf:${process.env.DB_PASSWORD}@cluster.jf1b1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster`);
 
+app.use(session({
+    secret: "senhaSecretaDoPedro123",
+    resave: false,
+    saveUninitialized: false
+}));    
+
 app.use(express.static(path.join(__dirname, '../src')))
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../src/index.html'))
 });
 
-app.get('/new', (req, res) => {
+app.get('/new', verifySession, (req, res) => {
     res.sendFile(path.join(__dirname, '../src/new.html'));
 });
 
@@ -26,6 +41,8 @@ app.listen(PORT, () => {
     console.log(`Server running on PORT: ${PORT}`)
 })
 
+
+///Registro
 app.post('/register', async (req, res) => {
     const { user, password } = req.body;
     const existingUser = await Login.findOne({ user });
@@ -40,6 +57,8 @@ app.post('/register', async (req, res) => {
     res.json(saveLogin);
 });
 
+
+///Login
 app.post('/login', async (req, res) => {
     const { user, password } = req.body;
     const loginUser = await Login.findOne({ user })
@@ -53,6 +72,8 @@ app.post('/login', async (req, res) => {
     if(!validation) {
         return res.status(400).json({ error: 'Senha incorreta' });
     }
+
+    req.session.user = loginUser.user
 
     res.json({ message: 'Login bem-sucedido' });
 });
